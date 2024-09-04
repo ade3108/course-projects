e48bb0ecccda387b5725067e2acf58e26e68ebba
ade3108 <adeliab@uio.no>
2024-09-04 08:10:27 +0000

    Differentiate done

--- projects/differentiate.py
+++ projects/differentiate.py
@@ -2,10 +2,19 @@ import numpy as np
 
 
 def differentiate(u, dt):
-    pass
+    d_n = np.zeros(len(u))
+    d_n[0]= (u[1] - u[0]) / dt
+    for i in range(1, len(u)-1):
+        d_n[i]=(u[i+1]-u[i-1])/(2*dt)
+    d_n[-1] = (u[-1] - u[-2]) / dt    
+    return d_n
 
 def differentiate_vector(u, dt):
-    pass
+    d= np.zeros(len(u))
+    d[0]= (u[1] - u[0]) / dt
+    d[1:-1] = (u[2:] -u[0:-2])/(2*dt)
+    d[-1] = (u[-1] - u[-2]) / dt
+    return d
 
 def test_differentiate():
     t = np.linspace(0, 1, 10)
@@ -13,7 +22,7 @@ def test_differentiate():
     u = t**2
     du1 = differentiate(u, dt)
     du2 = differentiate_vector(u, dt)
-    assert np.allclose(du1, du2)
+    assert np.allclose(du1,du2)
 
 if __name__ == '__main__':
     test_differentiate()
